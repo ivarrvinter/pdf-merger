e8ee8a7d743d594fb8492f9512e0087596d765e9
Ívarr Vinter <109881262+ivarrvinter@users.noreply.github.com>
2023-05-19 17:41:43 +0330

    Updated main.py
    Fixed the bug that created an empty pdf instead of merging PDFs

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import os
 import PyPDF2
 from PyQt5.QtWidgets import QApplication, QFileDialog
-
+import sys
 
 app = QApplication([])
 files, _ = QFileDialog.getOpenFileNames(None, "Select PDF Files", os.path.expanduser("~"), "PDF Files (*.pdf)")
+
+if not files:
+    print("No files selected.")
+    exit()
+
 merged_pdf = PyPDF2.PdfMerger()
 
 for file_name in files:
-    with open(file_name, "rb") as pdf_file:
-        merged_pdf.append(pdf_file)
+    try:
+        with open(file_name, "rb") as pdf_file:
+            merged_pdf.append(pdf_file)
+            print(f"Added {file_name}")
+    except IOError:
+        print(f"Error opening {file_name}")
 
-with open("merged.pdf", "wb") as merged_file:
-    merged_pdf.write(merged_file)
+output_path = "merged.pdf"
+
+try:
+    with open(output_path, "wb") as merged_file:
+        merged_pdf.write(merged_file)
+    print(f"Merge successful. Output saved to {output_path}")
+except IOError:
+    print("Error saving merged PDF.")
+
+sys.exit(app.exec_())
